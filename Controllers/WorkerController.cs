3026299e49c050560a9c7df915bbca54aa4903a9
sergio <sdlbsso@gmail.com>
2024-04-17 10:56:15 -0300

    crud completo

--- Controllers/WorkerController.cs
+++ Controllers/WorkerController.cs
@@ -30,19 +30,109 @@ namespace Redis.Controllers
                 };
                 return Ok(resp);
             }
-            catch (KeyNotFoundException)
+            catch (Exception ex)
             {
-
-                throw;
-            }
-            catch (Exception)
-            {
-
-                throw;
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = "Ocurrió un error interno" });
             }
         }
 
 
+        [HttpGet("Id:int")]
+        public async Task<IActionResult> Get(int Id)
+        {
+            try
+            {
+                ResponseDTO<WorkerDTO> response = new ResponseDTO<WorkerDTO>()
+                {
+                    Success = true,
+                    Data = await _workerService.GetById(Id),
+                    Message = "Datos del trabajador"
+                };
+                return Ok(response);
+            }
+            catch (KeyNotFoundException)
+            {
+
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = $"No existe el id {Id}" });
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = "Ocurrió un error interno" });
+            }
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> New(WorkerDTO worker)
+        {
+            try
+            {
+                ResponseDTO<WorkerDTO> response = new ResponseDTO<WorkerDTO>();
+
+                await _workerService.New(worker);
+
+                response.Success = true;
+                response.Data = worker;
+                response.Message = "Creado";
+
+                return Ok(response);
+
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = "Ocurrió un error interno" });
+            }
+        }
+
+
+        [HttpPut("Id:int")]
+        public async Task<IActionResult> Update(WorkerDTO worker, int Id)
+        {
+            try
+            {
+                ResponseDTO<bool> response = new ResponseDTO<bool>();
+                await _workerService.Update(worker, Id);
+                response.Success = true;
+                response.Data = true;
+                response.Message = "Actualizado";
+                return Ok(response);
+            }
+            catch (KeyNotFoundException)
+            {
+                return StatusCode(404, new ResponseDTO<bool> { Success = false, Message = $"No existe el id {Id}" });
+            }
+            catch (Exception)
+            {
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = "Error interno" });
+            }
+        }
+
+
+        [HttpDelete("Id:int")]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            try
+            {
+                ResponseDTO<bool> response = new ResponseDTO<bool>();
+                response.Success = true;
+                response.Data = true;
+                response.Message = $"Eliminado id {Id}";
+
+                return Ok(response);
+            }
+            catch (KeyNotFoundException)
+            {
+                return StatusCode(404, new ResponseDTO<bool> { Success = false, Message = $"No existe el id {Id}" });
+            }
+            catch (Exception)
+            {
+
+                return StatusCode(500, new ResponseDTO<bool> { Success = false, Message = "Error interno" });
+            }
+        }
 
     }
 }
